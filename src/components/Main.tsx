64551863e03b090d1d0f22d2a84c670fc46e214c
nisyuu <37263474+nisyuu@users.noreply.github.com>
2024-07-23 19:56:13 +0900

    タスク一覧取得機能の実装Part1

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,20 @@
+import { jwtDecode } from "jwt-decode";
 import Header from "./Header"
 import TaskTable from "./TaskTable"
+import { Payload } from "../types/payload";
+import { useQuery } from "@apollo/client";
+import { Task } from "../types/task";
+import { GET_TASKS } from "../queries/taskQueries";
 
 const Main = () => {
+	const token = localStorage.getItem("token");
+	const decodedToken = jwtDecode<Payload>(token!);
+	const userId = decodedToken.sub;
+
+	const { loading, data, error } = useQuery<{getTasks: Task[]}>(GET_TASKS, {
+		variables: { userId },
+	});
+	console.log(data);
 	return (
 		<>
 			<Header />
